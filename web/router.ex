76c50c98a3a2fce7f47e0f2e42661cd8f327cc59
Helder Alves <helder@brassun.com.br>
2021-02-26 17:31:37 -0300

    Adicionado variáveis de ambiente, em config e rotas

--- web/router.ex
+++ web/router.ex
@@ -2,22 +2,30 @@ defmodule Discuss.Router do
   use Discuss.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", Discuss do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    resources "/", TopicController
+    resources("/", TopicController)
   end
+
+  scope "/auth", Discuss do
+    pipe_through(:browser)
+
+    get("/:provider", AuthController, :request)
+    get("/:provider/callback", AuthController, :callback)
+   end
 
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
